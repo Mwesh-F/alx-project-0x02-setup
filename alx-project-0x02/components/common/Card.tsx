dbcaec76235e96f37894ef3f76573552f3a9e912
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 19:47:08 +0300

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,1 +1,13 @@
+import { CardProps } from "@/interfaces";
+
+const Card = ({ title, content }: CardProps) => {
+  return (
+    <div className="bg-white rounded-xl shadow-md p-4 border border-gray-200">
+      <h2 className="text-lg font-bold mb-2">{title}</h2>
+      <p className="text-gray-700">{content}</p>
+    </div>
+  );
+};
+
+export default Card;
 
