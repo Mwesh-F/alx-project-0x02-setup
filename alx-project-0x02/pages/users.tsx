0104576af95b8d3b564a7c7d6dfc3bd8b9161236
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 20:24:17 +0300

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,1 +1,37 @@
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { type UserProps } from "@/interfaces";
+
+interface UsersPageProps {
+  users: UserProps[];
+}
+
+const UsersPage = ({ users }: UsersPageProps) => {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Users</h1>
+        <div className="grid gap-4 md:grid-cols-2">
+          {users.map((user) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
+export default UsersPage;
 
