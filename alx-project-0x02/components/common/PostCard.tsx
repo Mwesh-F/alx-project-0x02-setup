c145b2b7083a0d4e30ea338a18d2b2c41bff485a
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 20:16:36 +0300

    Update PostCard.tsx

--- alx-project-0x02/components/common/PostCard.tsx
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -1,1 +1,14 @@
+import { type PostProps } from "@/interfaces";
+
+const PostCard = ({ title, content, userId }: PostProps) => {
+  return (
+    <div className="border border-gray-300 p-4 rounded shadow-sm">
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      <p className="text-gray-700 mb-2">{content}</p>
+      <p className="text-sm text-gray-500">Posted by User {userId}</p>
+    </div>
+  );
+};
+
+export default PostCard;
 
