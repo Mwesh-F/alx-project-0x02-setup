1f90b4a551f4cc2765fd6aa0978287e7c3035c2e
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 20:21:19 +0300

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -14,3 +14,20 @@ export interface PostProps {
   content: string;
   userId: number;
 }
+
+export interface CardProps {
+  title: string;
+  content: string;
+}
+
+export interface ButtonProps {
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  label: string;
+}
+
+export interface PostProps {
+  title: string;
+  content: string;
+  userId: number;
+}
