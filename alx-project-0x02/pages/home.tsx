f2b922062980b754987dad03f4572a9865b46ac6
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 19:41:35 +0300

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,1 +1,15 @@
+import Header from "@/components/layout/Header";
 
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+        <p>This is the Home page of the ALX Project.</p>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
