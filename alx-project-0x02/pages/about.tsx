1ab6c2b1cc480a825aad43c18d9a39b8b30e9c7b
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 19:59:59 +0300

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,16 +1,20 @@
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 const AboutPage = () => {
   return (
     <>
       <Header />
       <main className="p-4">
-        <h1 className="text-2xl font-bold">About Us</h1>
-        <p>This page provides information about the ALX Project.</p>
+        <h1 className="text-2xl font-bold mb-4">About Page</h1>
+        <div className="space-x-4 space-y-4">
+          <Button size="small" shape="rounded-sm" label="Small Button" />
+          <Button size="medium" shape="rounded-md" label="Medium Button" />
+          <Button size="large" shape="rounded-full" label="Large Button" />
+        </div>
       </main>
     </>
   );
 };
 
 export default AboutPage;
-
