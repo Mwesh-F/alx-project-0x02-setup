0850c5526fc80b334d1de1cfb7e49dbd86e41427
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 20:13:40 +0300

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,1 +1,16 @@
+import Header from "@/components/layout/Header";
+
+const PostsPage = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Posts Page</h1>
+        <p>This is where posts will be displayed.</p>
+      </main>
+    </>
+  );
+};
+
+export default PostsPage;
 
