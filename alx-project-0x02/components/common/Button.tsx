17d1a857cefb8cca507c8b45d6cb8eb529a982ba
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 19:59:06 +0300

    Update Button.tsx

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,1 +1,20 @@
+import { type ButtonProps } from "@/interfaces";
+
+const sizeClasses = {
+  small: "px-2 py-1 text-sm",
+  medium: "px-4 py-2 text-base",
+  large: "px-6 py-3 text-lg",
+};
+
+const Button = ({ size, shape, label }: ButtonProps) => {
+  return (
+    <button
+      className={`bg-blue-600 text-white ${sizeClasses[size]} ${shape} hover:bg-blue-700 transition`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
 
