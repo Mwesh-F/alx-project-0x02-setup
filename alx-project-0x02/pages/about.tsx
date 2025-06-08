af91ad4d37b1b3eaf95355d50a7550272a7041bf
Frida Mwende <mwendehfrida@gmail.com>
2025-06-08 19:42:09 +0300

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,1 +1,16 @@
+import Header from "@/components/layout/Header";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">About Us</h1>
+        <p>This page provides information about the ALX Project.</p>
+      </main>
+    </>
+  );
+};
+
+export default AboutPage;
 
